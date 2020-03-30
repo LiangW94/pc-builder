1151c8ee0b51180507b8b80582b96abedac3c2ca
Liang <wl2cp3@gmail.com>
2020-03-30 13:00:38 +0800

    feature: add mongoose scheam types

--- src/db/types.js
+++ src/db/types.js
@@ -2,12 +2,21 @@
  * encapsulate model type
  */
 
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+
+// module.exports = {
+//   STRING: Sequelize.STRING,
+//   DECIMAL: Sequelize.DECIMAL,
+//   TEXT: Sequelize.TEXT,
+//   INTEGER: Sequelize.INTEGER,
+//   BOOLEAN: Sequelize.BOOLEAN
+// };
 
 module.exports = {
-  STRING: Sequelize.STRING,
-  DECIMAL: Sequelize.DECIMAL,
-  TEXT: Sequelize.TEXT,
-  INTEGER: Sequelize.INTEGER,
-  BOOLEAN: Sequelize.BOOLEAN
+  STRING: String,
+  DECIMAL: Number,
+  BOOLEAN: Boolean,
+  DATE: Date,
+  BUFFER: Buffer,
+  ARRAY: Array
 };
