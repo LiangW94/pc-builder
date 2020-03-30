8e57c4a8dc1e257fd8dab2f5fe90a4ce14d7040f
Liang <wl2cp3@gmail.com>
2020-03-30 13:03:20 +0800

    feature: add fetch cpu data controller

--- src/controller/msy.js
+++ src/controller/msy.js
@@ -1,9 +1,8 @@
-const CpuModel = require('../db/schema/cpu');
-
 /**
  * @description fetch data controller
  */
 
+const CpuModel = require('../db/schema/cpu');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
 const { scrapProductDetail } = require('../scraperService/scraper');
 const { MSY_CONFIG } = require('../utils/constants');
@@ -12,12 +11,48 @@ const {
   updateData,
   findOneOrUpdate
 } = require('../services/msy');
-const { cpuDataMapper } = require('../services/formatter');
+const {
+  cpuDataMapper,
+  motherboardDataMapper
+} = require('../services/formatter');
 
+async function fetchCpuDataMongoose() {
+  try {
+    const productList = await scrapProductDetail(MSY_CONFIG.CATEGORY_ID.CPU);
+    const formattedData = cpuDataMapper(productList.data);
+    const result = await findOneOrUpdate(formattedData, CpuModel);
+    return new SuccessModel(result);
+  } catch (error) {
+    console.log(error.message);
+    return new ErrorModel({
+      errno: 1,
+      message: error.message
+    });
+  }
+}
+
+async function fetchMotherboardDataMongoose() {
+  try {
+    const productList = await scrapProductDetail(
+      MSY_CONFIG.CATEGORY_ID.MOTHERBOARD
+    );
+    const formattedData = motherboardDataMapper(productList.data);
+    const result = await findOneOrUpdate(formattedData, CpuModel);
+    return new SuccessModel(result);
+  } catch (error) {
+    console.log(error.message);
+    return new ErrorModel({
+      errno: 1,
+      message: error.message
+    });
+  }
+}
+
+// Legacy method
 async function fetchCpuDataSequelize() {
   try {
     const productList = await scrapProductDetail(MSY_CONFIG.CATEGORY_ID.CPU);
-    const formattedCpuData = cpuDataMapper(productList.data);
+    const formattedData = cpuDataMapper(productList.data);
 
     const createdProduct = await findOrCreate(formattedCpuData);
     const updatedProduct = await updateData(formattedCpuData);
@@ -31,22 +66,8 @@ async function fetchCpuDataSequelize() {
   }
 }
 
-async function fetchCpuDataMongoose() {
-  try {
-    const productList = await scrapProductDetail(MSY_CONFIG.CATEGORY_ID.CPU);
-    const formattedCpuData = cpuDataMapper(productList.data);
-    const result = await findOneOrUpdate(formattedCpuData);
-    return new SuccessModel(result);
-  } catch (error) {
-    console.log(error.message);
-    return new ErrorModel({
-      errno: 1,
-      message: error.message
-    });
-  }
-}
-
 module.exports = {
   fetchCpuDataSequelize,
-  fetchCpuDataMongoose
+  fetchCpuDataMongoose,
+  fetchMotherboardDataMongoose
 };
--- src/services/formatter.js
+++ src/services/formatter.js
@@ -25,4 +25,27 @@ function cpuDataMapper(rawData) {
   });
 }
 
-module.exports = { cpuDataMapper };
+function motherboardDataMapper(rawData) {
+  return rawData.map(product => {
+    return {
+      name: product['productName'],
+      category: 'cpu',
+      brand:
+        product['Brand'] ||
+        product['CPU Brand'] ||
+        product['productName'].split(' ')[0] ||
+        'unbranded',
+      price: product['price'],
+      image: product['image'],
+      retailerId: product['sku_id'],
+      retailerSKU: product['sku'],
+      tpd: product['TDP (Thermal Design Power)'],
+      //   performanceLevel: ,
+      inStock: product['inStock'],
+      socket: product['AMD CPU Socket'] || product['Intel CPU Socket'] || '',
+      integratedGPU: product['Discrete Graphics Required'] === 'No'
+    };
+  });
+}
+
+module.exports = { cpuDataMapper, motherboardDataMapper };
