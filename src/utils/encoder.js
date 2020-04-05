5280cfb888afd89391136e895c406f7f50c08df7
Liang <wl2cp3@gmail.com>
2020-04-05 14:16:20 +0800

    feature: add encode image function

---
+++ src/utils/encoder.js
@@ -0,0 +1,22 @@
+const request = require('request').defaults({ encoding: null });
+
+const encodeImage = (url) =>
+  new Promise((res, rej) => {
+    try {
+      request.get(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          data =
+            'data:' +
+            response.headers['content-type'] +
+            ';base64,' +
+            Buffer.from(body).toString('base64');
+          console.log(data);
+          res(data);
+        }
+      });
+    } catch (error) {
+      rej(error);
+    }
+  });
+
+module.exports = { encodeImage };
